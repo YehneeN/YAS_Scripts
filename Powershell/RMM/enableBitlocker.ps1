ba894a761a1fad267bc29b553be0f1ce16f9544d
YehneeN <rfontaine@etik.com>
2024-09-26 18:15:03 +0200

    ...

---
+++ Powershell/RMM/enableBitlocker.ps1
@@ -0,0 +1,22 @@
+##############################################################################################################
+#
+# YehneeN - 2024
+##############################################################################################################
+
+# Var
+$LogSource = "INTECH Events"
+
+# Vérification du module
+if (-not (Get-Module -ListAvailable -Name Bitlocker)) {
+    New-EventLog -LogName "Application" -Source $LogSource -ErrorAction SilentlyContinue
+    Write-EventLog -LogName "Application" -Source $LogSource -EventID 11002 -EntryType Error -Message "Module bitlocker disabled or inexistant on this device."
+    exit
+}
+
+# Chemin du répertoire partagé pour sauvegarder la RecoveryKey
+$netPath = {[sharedFolder]}
+
+# Activation Bitlocker en utilisant le TPM + sauvegarde de la clé 
+Enable-Bitlocker -MountPoint "C:" -TpmProtector -RecoveryKeyPath $netPath
+New-EventLog -LogName "Application" -Source $LogSource -ErrorAction SilentlyContinue
+Write-EventLog -LogName "Application" -Source $LogSource -EventID 11005 -EntryType Error -Message "Bitlocker has been enabled on the system drive."
